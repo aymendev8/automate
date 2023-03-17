48b16001e391bc473cd596e360984b71f2422952
Aymen Kadri <aymenkadri5@gmail.com>
2023-03-17 15:43:45 +0100

    fix bug

--- main.py
+++ main.py
@@ -1,27 +1,29 @@
-import os
 import graphviz as gv
 
+alphabet = []
+etats = []
+etats_initiaux = []
+etats_finaux = []
+regles = []
+les_regles = []
 
-def creer_graphe():
+graphe = gv.Digraph()
+
+
+def lecture_fichier():
     with open("automate.txt", 'r') as f:
         alphabet = f.readline().strip().split()
-        etats = f.readline().strip().split()
+        etats = f.readline().strip().split(",")
         etats_initiaux = f.readline().strip().split()
-        etats_finaux = f.readline().strip().split()
+        etats_finaux = f.readline().strip().split(",")
         regles = f.read().strip()
         segments = regles.split(',')
-        les_regles = []
         for segment in segments:
             elements = segment.split('>')
             les_regles.append(elements)
 
-    graphe = gv.Digraph()
 
-    # Ajouter les états finaux en forme de double cercle
-    # for etat in etats_finaux:
-    #     graphe.node(etat, shape='doublecircle')
-
-    # Afficher les états accessibles
+def etats_accessibles():
     accessibles = set()
     accessibles.add(etats_initiaux[0])
     for regle in les_regles:
@@ -30,7 +32,8 @@ def creer_graphe():
             accessibles.add(destination)
     print(f"États accessibles : {accessibles}")
 
-    # Afficher les états co-accessibles
+
+def etats_co_accessibles():
     co_accessibles = set(etats_finaux)
     for regle in les_regles:
         origine, c, destination = regle
@@ -38,23 +41,19 @@ def creer_graphe():
             co_accessibles.add(origine)
     print(f"États co-accessibles : {co_accessibles}")
 
-    # Ajouter les états
-    # for etat in etats:
-    #     if etat in etats_initiaux:
-    #         graphe.node(etat, shape='circle')
-    #         graphe.edge('dummy', etat)
-    #     elif etat in etats_finaux:
-    #         graphe.node(etat, shape='doublecircle')
-    #     else:
-    #         graphe.node(etat, shape='circle')
 
-    for regle in les_regles:
-        origine, c, destination = regle
-        #graphe.edge(origine, destination, label=c)
+graphe.node('dummy', shape='point')
+for etat in etats:
+    if etat in etats_initiaux:
+        graphe.node(etat, shape='circle')
+        graphe.edge('dummy', etat)
+    elif etat in etats_finaux:
+        graphe.node(etat, shape='doublecircle')
+    else:
+        graphe.node(etat, shape='circle')
 
-    # Rendre le graphique
-    graphe.render('graph', view=True)
-    os.system('start graph.pdf')
+for regle in les_regles:
+    origine, c, destination = regle
+    graphe.edge(origine, destination, label=c)
 
-
-creer_graphe()
+graphe.render('graph', view=True)
